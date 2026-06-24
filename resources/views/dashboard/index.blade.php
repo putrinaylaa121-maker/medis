e0ba360deb2a3aebf62da93652f0e16f3a7d3774
Hani Hidayati <hanihidaay@gmail.com>
2026-06-24 14:59:40 +0700

    Menyelesaikan fitur dashboard tugas Mahasiswa B

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Dashboard</title>
+</head>
+<body>
+    <h1>Halaman Dashboard Tugas Saya</h1>
+    <p>Selamat datang di halaman dashboard project kelompok.</p>
+</body>
+</html>
